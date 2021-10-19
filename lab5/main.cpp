4889bb4ca13ecfcc0705a2d87b6c2d8c1816c85f
QElderDelta <egorzhivalev@gmail.com>
2021-10-19 21:27:02 +0300

    add printing of analytic solution

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -29,6 +29,7 @@ int main(int argc, const char *argv[]) {
     std::ofstream f1("expl.txt", std::ios::trunc);
     std::ofstream f2("impl.txt", std::ios::trunc);
     std::ofstream f3("exim.txt", std::ios::trunc);
+    std::ofstream f4("analytic.txt", std::ios::trunc);
     TaskData d{
             left_constraint,
             0, //alpha
@@ -43,14 +44,20 @@ int main(int argc, const char *argv[]) {
             0, //x_left
             M_PI / 2, //x_right
             0, //t_bottom
-            1, //t_top
+            10, //t_top
             free_func
     };
     auto ex_res = CrankNicolsonSolution(d, 0);
     f1 << ex_res << '\n';
     auto imp_res = CrankNicolsonSolution(d, 1);
     f2 << imp_res << '\n';
-    auto crank_nicl_res = CrankNicolsonSolution(d);
+    auto crank_nicl_res = CrankNicolsonSolution(d, 0.5);
     f3 << crank_nicl_res << '\n';
+    for(int i = 0; i < (d.t_top - d.t_bottom) / d.tau; ++i) {
+        for(int j = 0; j < (d.x_right - d.x_left) / d.h + 1; ++j) {
+            f4 << sin(GetIthPoint(d.t_bottom, d.tau, i)) * cos(GetIthPoint(d.x_left, d.h, j)) << ' ';
+        }
+        f4 << '\n';
+    }
     return 0;
 }
